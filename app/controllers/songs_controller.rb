ee65ace4e1b7e1c7290098b94d85142e72a8be76
Drew Thompson <drewallynthompson@gmail.com>
2020-12-06 07:32:49 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path, alert: "Artist not found"
     else
-      @song = Song.new(params[:author_id])
+      @song = Song.new(params[:artist_id])
     end
   end
 
